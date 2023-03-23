a3328e133bff0f2fdc24047f41c338dd73a7c68a
andriy.mereshchak@uconn.edu.com <andriy.mereshchak@uconn.edu>
2023-03-23 19:10:34 -0400

    Fixed controller to show routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT
+const mainController = require('./controllers/main')
 
 const methodOverride = require('method-override')
 
@@ -9,6 +10,7 @@ app.use(methodOverride('_method'))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use('/public', express.static('public'))
+app.use(mainController)
 
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
