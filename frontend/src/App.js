35e575b98c53430f103fbfa04b0d87308460a903
andriy.mereshchak@uconn.edu.com <andriy.mereshchak@uconn.edu>
2023-03-24 15:34:37 -0400

    Frontend links created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom'
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+
 
 function App() {
+  const URL = 'https://project-lab-backend.herokuapp.com/'
+  
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='about' element={<About URL={URL} />} />
+        <Route eaxct path='projects' element={<Projects URL={URL} />} />
+      </Routes>
       <Footer />
     </div>
   );
