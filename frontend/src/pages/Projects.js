5bd83b7ce005b60d5d21299f80718d96ea0a13d4
andriy.mereshchak@uconn.edu.com <andriy.mereshchak@uconn.edu>
2023-03-24 19:12:48 -0400

    Frontend complete and ready to deploy

--- frontend/src/pages/Projects.js
+++ frontend/src/pages/Projects.js
@@ -1,8 +1,33 @@
+import { useState, useEffect } from 'react'
+
 const Projects = (props) => {
+    const [projects, setProjects] = useState(null)
+    const updateProjects = async () => {
+        const response = await fetch(props.URL + 'projects')
+        console.log(response)
+        const data = await response.json()
+        setProjects(data)
+    }
+    // eslint-disable-next-line
+    useEffect(() => updateProjects(), [])
 
-    return(
-        <h1>Projects</h1>
-    )
-}
-
-export default Projects
+      const loaded = () => {
+        return projects.map((project) => (
+          <div>
+            <h1>{project.name}</h1>
+            {/* eslint-disable-next-line */}
+            <img src={project.image} />
+            <a href={project.git}>
+              <button>Github</button>
+            </a>
+            <a href={project.live}>
+              <button>live site</button>
+            </a>
+          </div>
+        ));
+      };
+    
+      return projects ? loaded() : <h1>Loading...</h1>;
+    }
+    
+    export default Projects;
