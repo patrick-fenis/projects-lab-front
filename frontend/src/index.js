5ab3b0e1edc4035f0041154fcf255ed4c580dfdf
andriy.mereshchak@uconn.edu.com <andriy.mereshchak@uconn.edu>
2023-03-24 00:57:46 -0400

    Directories Created

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,12 +9,14 @@ import './styles.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
